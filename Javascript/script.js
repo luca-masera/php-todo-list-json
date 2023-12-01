d3e7bfea0192de1b922dc497ee32d9ffc8d5bb14
luca-masera <lucamasera1995@gmail.com>
2023-12-01 15:17:48 +0100

    initial commit

---
+++ Javascript/script.js
@@ -0,0 +1,69 @@
+const { createApp } = Vue
+
+
+createApp({
+
+    data() {
+
+        return {
+            lista: [
+
+                {
+                    id: 1,
+                    text: 'Comprare la zucca',
+                    done: true
+                },
+                {
+                    id: 2,
+                    text: 'Comprare il vestito',
+                    done: false
+                },
+                {
+                    id: 3,
+                    text: 'Comprare i dolcetti',
+                    done: true
+                },
+                {
+                    id: 4,
+                    text: 'Comprare le decorazioni',
+                    done: true
+                },
+                {
+                    id: 5,
+                    text: 'Comprare due birre',
+                    done: false
+                }
+            ],
+
+            lastId: 5,
+            todoTextNew: ''
+
+
+        }
+    },
+
+    methods: {
+
+        remove(id) {
+            this.lista.splice(id, 1)
+        },
+
+        aggiungi() {
+            this.lastId++;
+
+            const newObj = {
+                id: this.lastId,
+                text: this.todoTextNew,
+                done: true
+            }
+            this.lista.push(newObj),
+                this.todoTextNew = ''
+        },
+
+        bonus2(id) {
+            this.lista[id].done = !this.lista[id].done
+        }
+
+    }
+
+}).mount('#app')
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <script src="https://unpkg.com/axios@1.1.2/dist/axios.min.js"></script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <title>Vue To Do List</title>
+</head>
+
+<body>
+    <div id="app">
+        <div class="container">
+            <header class="text-center pt-4">
+                <img class="w-25" src="img/ita.png" alt="">
+                <div class="pt-5">
+                    <input type="text" class="form-control w-25 d-inline-block mx-3" v-model="todoTextNew"
+                        @keyup.enter="aggiungi">
+                    <button class="btn btn-danger" @click="aggiungi">addList</button>
+                </div>
+            </header>
+            <main>
+                <ul class="list-group">
+                    <li class="list-group-item list-group-item-action d-flex justify-content-between"
+                        v-for="(todo, index) in lista" :key="todo.id">
+                        <span :class="{'done': todo.done}" @click="bonus2(id)">{{todo.text}}</span>
+                        <i class="fa-solid fa-trash" @click="remove(index)"></i>
+
+
+                    </li>
+
+                </ul>
+
+
+            </main>
+
+        </div>
+
+    </div>
+
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="Javascript/script.js"></script>
+</body>
+
+</html>
+{{}}
